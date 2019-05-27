cae71087f2954c96074b12c83ce53cec1a9768cb
adavecohen95 <51063091+adavecohen95@users.noreply.github.com>
2019-05-26 17:19:57 -0700

    Add files via upload

---
+++ git_spring_start_Jew_Events/complete/src/main/java/calendar/MainController.java
@@ -0,0 +1,35 @@
+package calendar;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import services.FacebookEventService;
+
+@RestController
+public class MainController {
+
+    private static final String template = "Hello, %s!";
+    private static final String boratTemplate = "What kind of %s is this";
+    private static final FacebookEventService facebookEventService = new FacebookEventService();
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public TestObject greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new TestObject(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+
+    @RequestMapping("/borat")
+    public TestObject borat(@RequestParam(value="animal", defaultValue="Dog") String animal) {
+        return new TestObject(counter.incrementAndGet(),
+                String.format(boratTemplate, animal));
+    }
+
+    @RequestMapping("/serviceCall")
+    public TestObject fbServiceCall() {
+        return new TestObject(counter.incrementAndGet(),
+                String.format(template, facebookEventService.testMethod()));
+    }
+
+}
---
+++ git_spring_start_Jew_Events/complete/src/main/java/services/FacebookEventService.java
@@ -0,0 +1,11 @@
+package services;
+
+public class FacebookEventService {
+
+
+    public String testMethod() {
+        return "Service calls work ";
+    }
+
+
+}
