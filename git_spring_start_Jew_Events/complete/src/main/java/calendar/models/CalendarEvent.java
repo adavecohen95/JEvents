f4f5d83bc6d00828e39824f3faf239b4eeca3f95
adcohen <adcohen@linkedin.com>
2019-06-18 15:53:41 -0700

    Calendar Event + Facebook Event merge

--- git_spring_start_Jew_Events/complete/src/main/java/calendar/models/CalendarEvent.java
+++ git_spring_start_Jew_Events/complete/src/main/java/calendar/models/CalendarEvent.java
@@ -1,40 +1,38 @@
 package calendar.models;
 
+import calendar.models.facebookEvents.FacebookPlace;
 import com.google.api.client.util.DateTime;
+import com.google.gson.Gson;
 
 
 public class CalendarEvent {
+
+  /* facebook event fields that are imported from facebook rest response */
+  public String description;
+  public String end_time;
+  public String name;
+  public FacebookPlace place;
+  public String start_time;
+  public long id;
+  public String rsvp_status;
+  /* End facebook event fields */
+
+
+  /* Obsolete fields */
   public String facebookEventId;
+  public DateTime startTime;
+  public DateTime endTime;
+  //public String description;
+  public String title;
+  /*End Obsolete fields */
+
   public String googleEventId;
   // Use this to detect if the event has been modified. Same event ID but
   // different event Etag implies that the event's details have been changed.
   public String googleEventEtag;
-
-  public DateTime startTime;
-  public DateTime endTime;
-
-  public String description;
-  public String title;
-
   public String googleEventUrl;
 
   public String toString() {
-    return "{\nfbeid: "
-        + facebookEventId
-        + "\ngeid: "
-        + googleEventId
-        + "\ngoogleetag: "
-        + googleEventEtag
-        + "\nstart_t: "
-        + startTime.toString()
-        + "\nend_t: "
-        + endTime.toString()
-        + "\ndescription: "
-        + description
-        + "\ntitle: "
-        + title
-        + "\nURL: "
-        + googleEventUrl
-        + "\n";
+    return new Gson().toJson(this);
   }
 }
